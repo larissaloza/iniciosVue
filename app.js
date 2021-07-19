5640830f9406189bb339a099b6cf1bd3e3b2b728
larissaloza <58893049+larissaloza@users.noreply.github.com>
2021-07-19 14:31:41 -0700

    Update app.js

--- app.js
+++ app.js
@@ -10,12 +10,18 @@ const app = Vue.createApp({
  },
 
  methods: {
-getUser () {
-  this.firstName='Sam'
-  this.lastName='Rosa'
-  this.email='samrosa@yahoo.com'
-  this.gender='female'
-  this.picture= 'https://randomuser.me/api/portraits/women/75.jpg'
+
+async getUser () {
+const res= await fetch('https://randomuser.me/api')
+const { results }= await res.json ()
+
+// console.log(results)
+
+  this.firstName= results [0].name.first
+  this.lastName=results [0].name.last
+  this.email= results [0]. email
+  this.gender=results [0]. picture
+  this.picture= results[0].picture.large
 }
  }
 
